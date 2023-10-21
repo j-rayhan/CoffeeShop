2c4b57f7298c7cd2b38fde5b4171643b71734e8f
bss-johir <bss.johir@gmail.com>
2023-10-21 19:38:29 +0600

    UI: tailwind config app css lib

--- app/view/screen/Home.tsx
+++ app/view/screen/Home.tsx
@@ -4,7 +4,7 @@ import {View, Text} from 'react-native';
 const Home = () => {
   return (
     <View className="flex-1 justify-center">
-      <Text className="text-2xl text-black dark:text-white">
+      <Text className="text-t-6 font-poppins text-dark dark:text-gray">
         Edit Home.tsx to change this screen.
       </Text>
     </View>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,30 @@
 module.exports = {
   content: ['./App.{js,jsx,ts,tsx}', './app/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      primary: '#D17842',
+      secondary: '#DC3535',
+      gray: '#52555A',
+      white: '#FFFFFF',
+      'white-light': '#AEAEAE',
+      dark: '#0C0F14',
+      'dark-light': '#252A32',
+    },
+    fontFamily: {
+      poppins: [
+        'Poppins',
+        {
+          fontFeatureSettings: '"cv11", "ss01"',
+          fontVariationSettings: '"opsz" 32',
+        },
+      ], // Adds a new `font-poppins` class
+    },
+    extend: {
+      fontSize: {
+        't-1': ['1.75rem', '2rem'],
+        't-6': ['.625rem', '.875rem'],
+      },
+    },
   },
   plugins: [],
-}
-
+};
