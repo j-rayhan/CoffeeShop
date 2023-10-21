64ff92e80e87ae76c44985d80958a02ad4928501
bss-johir <bss.johir@gmail.com>
2023-10-21 13:12:32 +0600

    UI: android app css lib

---
+++ app/view/screen/Cart.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Cart = () => {
+   return (
+       <View>
+           <Text>
+             Edit Cart.tsx to change this screen.
+           </Text>
+       </View>
+   );
+};
+
+export default Cart;
---
+++ app/view/screen/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Home = () => {
+  return (
+    <View className="flex-1 justify-center">
+      <Text className="text-2xl text-black dark:text-white">
+        Edit Home.tsx to change this screen.
+      </Text>
+    </View>
+  );
+};
+
+export default Home;
